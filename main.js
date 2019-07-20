a8149fb948fdaee7fde96f9bae09ddfc068349be
JAJI <jajmantobi@gmail.com>
2019-07-20 12:12:28 +0100

    add modal popup

---
+++ main.js
@@ -0,0 +1,15 @@
+let modal = document.querySelector(".modal");
+let trigger = document.querySelector(".button-register");
+let closeButton = document.querySelector(".close-button");
+
+const toggleModal = () => modal.classList.toggle("show-modal");
+
+const windowOnClick = (e) => {
+  if (e.target === modal) {
+    toggleModal();
+  }
+}
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
